35efee34a3cd44ad8a932fa91d4f325d8ef8c1bf
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-11 09:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ gsap.to(".about .btn",{scrollTrigger:{
 // contact mask image //
 const timeLine =gsap.timeline().from(".bus",{scale:2, duration:2}).to(".mask",{
     clipPath:"polygon(0% 0%,100% 0%,100% 100%,0% 100%)"
-},0).to(".mask",{scale:1,duration:1}).to(".ct-hover",{height:"100%",duration:1,ease:"Power3.inOut"}).from(".upperPart",{opacity:0, duration:1,ease:"Power3.inOut"}).from(".lowerPart",{opacity:0, duration:1,ease:"Power3.inOut"})
+},0).to(".mask",{scale:1,duration:1}).to(".ct-hover",{height:"100%",duration:1,ease:"Power3.inOut"}).from(".contact-title",{scale:0,opacity:0,duration:1,ease:"Power3.inOut"}).from(".upperPart",{opacity:0, duration:1,ease:"Power3.inOut"}).from(".lowerPart",{opacity:0, duration:1,ease:"Power3.inOut"})
 ScrollTrigger.create({
     trigger:".contact",
     start:"top 20%",
