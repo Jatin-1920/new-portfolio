b8820ed003ab8c5714ba2287fddedbf901cb445d
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-12 12:46:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ gsap.fromTo([firstNav,secondNav],{height:"0vh",skewY:3,transformOrigin:"right to
 }})
     gsap.to(".nav-social",{scale:1,opacity:1,delay:.8, duration:.2,ease:"Power3.inOut"})
 } else{
-    gsap.to(".nav-social",{scale:0, opacity:0,ease:"Power3.inOut"},0)
     gsap.to(primaryNav,{display:"none",delay:.65})
     gsap.to([secondNav,firstNav],{height:"0vh",duration:.65,ease:"Power3.inOut",stagger:{
         amount:.085
