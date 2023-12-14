f878628dc0b912a0259861494cb17385bf7d4dc2
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-14 13:58:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -177,6 +177,6 @@ const firstFooter  = document.querySelector(".first-footer")
 const footer = document.querySelector(".footer")
 gsap.to([firstFooter,secondFooter],{height:"100%",stagger:{amount:.075}, duration:.8,ease:"Power3.inOut",scrollTrigger:{
     trigger:".footer",
-    start:"top top",
+    start:"top 30%",
     end:"bottom bottom"
 }})
