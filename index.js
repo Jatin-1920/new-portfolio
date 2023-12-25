d9f845ce03f46ac7aedb0b72993efd453818730c
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-25 11:29:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,11 @@ ScrollTrigger.create({
 
 
     //  TEXT ANIMATION //
-
+TweenMax.staggerFrom(".about-img",{width:0%,duration:.8,scrollTrigger:{
+    trigger:".about-img",
+    start:"top 20%",
+    end:"bottom bottom",
+}},0.04)
 
 const firstLine = document.querySelectorAll(".about-content h2 span")
 const secondLine =document.querySelector(".about-title span")
