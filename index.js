9c8a60c08252800ac9139bf95786e7d50700fe6d
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-25 20:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ ScrollTrigger.create({
 
 
     //  TEXT ANIMATION //
-gsap.to(".about-img div",1,{y:"-100%",ease:"power3.inOut",stagger:{
+gsap.to(".about-img div",1,{y:"-100%",stagger:{
     amount:0.15
 },scrollTrigger:{
     trigger:".about-img",
