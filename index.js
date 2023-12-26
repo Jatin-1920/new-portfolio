0b38fc9e9200edecf61f6a084983547e3c610ae8
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-26 19:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+const lenis = new Lenis()
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
 const left = document.getElementById("left")
 const header = document.querySelector(".header")
 let mouse = document.querySelector(".mouse-event")
