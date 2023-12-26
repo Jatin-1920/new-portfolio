1ebb638cdfe860c7e451a5523cf4e47d1196ec2a
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2023-12-26 09:54:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,13 +225,3 @@ canvas.addEventListener("mouseout",(e)=>{
     clearCanvas()
 })
 
-gsap.to(".about-img", {
-  scrollTrigger: {
-    scrub: true,
-      trigger:".about-img",
-      start:"top top",
-      end:"bottom bottom"
-  }, 
-  y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
-  ease: "none"
-});
