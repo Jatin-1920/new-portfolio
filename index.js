80acc4bbb1ba572339e68671f24d07dbcfa4c654
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-02 21:16:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ gsap.to(".about-img", {
       scrub: true,
       pin:false
   }, 
-  y:"-10dvh",
+  y:"-10vh",
   ease: "none"
 });
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
