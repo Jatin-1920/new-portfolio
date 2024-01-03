0dbec6ba7d6967a9d51c896937a4952e80e0596c
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-03 11:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 const bodyScrollBar = Scrollbar.init(document.body, { damping: 0.1, delegateTo: document });
  
-bodyScrollBar.setPosition(0, 0);
-bodyScrollBar.track.xAxis.element.remove();
+
 
 // How to get them to work together
 ScrollTrigger.scrollerProxy("body", {
