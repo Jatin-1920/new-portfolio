f054300fe8ef00d9ae63ea215127d5a751228616
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-03 16:12:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 gsap.registerPlugin(ScrollTrigger)
 
+const lenis = new Lenis()
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
 
 // animations //
 // Web Intro Animation //
