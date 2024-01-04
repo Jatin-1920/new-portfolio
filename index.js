646a4151aad3e0b128b13e165dfced06cc123515
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-04 16:15:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -263,10 +263,11 @@ gsap.to(".about .btn",{scrollTrigger:{
 
 // date //
 const dates = document.querySelector(".date")
-
 const date = new Date()
 const year = date.getFullYear()
-dates.innerHTML = year;
+const month = date.getMonth() +1
+const dated = date.getDate() 
+dates.innerHTML = `${dated}-${month}-${year}`;
 let canvas = document.querySelector(".canvas")
 let footer = document.querySelector(".footer")
 let ctx = canvas.getContext("2d")
