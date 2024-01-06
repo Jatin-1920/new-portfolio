f5d2c687d9f1d209778de6873a630a60a0b28fbc
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-06 12:24:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ gsap.fromTo([firstNav,secondNav],{height:"0vh",skewY:3,transformOrigin:"right to
       
     gsap.to(".movie-mask",{scale:1,opacity:1,delay:.5, duration:.5,ease:"Power3.inOut"})
 } else{
-      movieState = false
+      movieState = !movieState;
       gsap.to(".movie-mask",{opacity:0,scale:0})
     gsap.to(primaryNav,{display:"none",delay:.65})
     gsap.to([secondNav,firstNav],{height:"0vh",duration:.65,ease:"Power3.inOut",stagger:{
