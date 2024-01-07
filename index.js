3cb7882e1dbc97a0d835dc182d4427711c694c66
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-07 09:18:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".gsapPin"),
   smooth: true,
   lerp:0.1,
+  multiplier:1.5,
   gestureDirection:"both",
 
   // for tablet smooth
