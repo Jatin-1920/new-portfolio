79810e63a77916cdd3543a65135772e20174c22f
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-07 08:49:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,35 @@
 
 gsap.registerPlugin(ScrollTrigger,ScrambleText)
 
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".smooth-scroll"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  
 // animations //
 // Web Intro Animation //
 
@@ -401,5 +430,7 @@ gsap.to(".about-img", {
   ease: "none"
 });
 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
+ScrollTrigger.refresh();
 
