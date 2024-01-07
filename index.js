7867fc578b2c9509b334fc8386358f86efc63ec1
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-07 20:37:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ ScrollTrigger.create({
 
 
   gsap.to(".div",{ease:"none",duration:1,yPercent:-100, stagger:{
-    amount:0.15
+    amount:0.1
   },scrollTrigger:{
     trigger:".about-img",
     start:"top 10%",
