41769eef272123f33ee45039784efbf39bda0e41
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-01-07 09:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger,ScrambleText)
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".gsapPin"),
   smooth: true,
-  lerp:1,
+  lerp:0.1,
   gestureDirection:"both",
 
   // for tablet smooth
